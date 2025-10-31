3c57d3988d2d928833cd06325d550ca89f51031e
shubhamjh4 <i.shubhamkumarjha@gmail.com>
2025-11-01 01:34:51 +0530

    Fix email timeout - make email sending non-blocking

--- visitor_portal/visitor_portal/settings.py
+++ visitor_portal/visitor_portal/settings.py
@@ -199,8 +199,8 @@ EMAIL_HOST = 'smtp.gmail.com'  # Gmail SMTP server
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 10  # Timeout after 10 seconds
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'kars4r2@gmail.com')  # Your email address
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'Cursor@1234')  # Your email app password
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')  # Your email address
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')  # Your email app password
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)  # Use EMAIL_HOST_USER as default
 
 # Password reset settings
