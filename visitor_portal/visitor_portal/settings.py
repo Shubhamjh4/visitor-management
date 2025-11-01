88e1f4a9ba17180f1d8941e15b9f74d92e39708a
shubhamjh4 <i.shubhamkumarjha@gmail.com>
2025-11-01 10:35:59 +0530

    fix bug

--- visitor_portal/visitor_portal/settings.py
+++ visitor_portal/visitor_portal/settings.py
@@ -201,11 +201,7 @@ EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 10  # Timeout after 10 seconds
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')  # Your email address
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')  # Your email app password
-<<<<<<< HEAD
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@eterna.com')  # Use noreply@eterna.com as default
-=======
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)  # Use EMAIL_HOST_USER as default
->>>>>>> 7aa375d6a09453d04bdc90291bebd061148f1635
 
-# Password reset settings
+# Password reset settings   
 PASSWORD_RESET_TIMEOUT = 3600  # 1 hour
